9cd6f3b8c4bf19ec816a660deac026a6bae50999
andriydzhrvts <andrii.dzhuravets@gmail.com>
2022-12-13 21:06:40 +0200

    indexOf + tests

--- task3/index.js
+++ task3/index.js
@@ -1,56 +1,23 @@
 const express = require("express");
 const PORT = 5000;
 const app = express();
-const target = 555;
+const fs = require("fs");
+
+const target = 911952;
 
 async function start() {
     try {
         app.listen(PORT, async () => {
             console.log("Server has been started");
-            const arr = generateNumbers();
+            const arr = JSON.parse(await fs.promises.readFile('./files/numbers'));
             console.time("getIndex");
-            console.log(getIndex(arr, target));
+            console.log(arr.indexOf(target));
             console.timeEnd("getIndex");
             process.exit(0);
         });
     } catch (e) {
         console.log(e);
     }
-}
-
-exports.generateNumbers = () => {
-    const res = []
-
-    for (let i = 0; i < 1000000; i++) {
-        res.push(Math.floor(Math.random() * (1000 + 1)));
-    }
-    res.sort();
-
-    return res;
-}
-
-const binarySearch = (arr, target) => {
-    let start = 0;
-    let end = arr.length - 1;
-
-    while (start <= end) {
-        const mid = Math.floor((start + end) / 2);
-
-        if (arr[mid] === target) {
-            return mid;
-        }
-
-        if (target < arr[mid]) {
-            end = mid - 1;
-        } else {
-            start = mid + 1;
-        }
-    }
-    return -1;
-}
-
-exports.getIndex = (arr, target) => {
-    return binarySearch(arr, target);
 }
 
 start();
--- task3/test/test.js
+++ task3/test/test.js
@@ -1,19 +1,35 @@
-var assert = require('assert');
-const { generateNumbers, getIndex } = require('../index');
+const assert = require('assert');
+const fs = require("fs");
+const { getIndex } = require('../utils/arraySearch');
 
-describe('Array', () => {
 
-    describe('Generate array', () => {
-        it('Generate 1.000.000 numbers array', () => {
-            assert.equal(generateNumbers().length, 1000000);
-        });
+describe('Array', async () => {
+    let arr;
+    beforeEach(async () => {
+        try {
+            arr = JSON.parse(await fs.promises.readFile('./files/numbers'));
+        } catch (error) {
+            console.log(error);
+        }
+
     });
 
-    describe('Index searching', () => {
-        const arr = generateNumbers();
-        const target = 2300;
+    describe('Index searching', async () => {
         it('Out of generated numbers range', () => {
+            const target = 1000000;
             assert.equal(getIndex(arr, target), -1);
+        });
+        it('Found 911952 in position 911992 (not first of duplicates)', () => {
+            const target = 911952;
+            assert.notEqual(getIndex(arr, target), 911992);
+        });
+        it('Found 911952 in position 911992', () => {
+            const target = 911952;
+            assert.equal(getIndex(arr, target), 911990);
+        });
+        it('Start of array', () => {
+            const target = 0;
+            assert.equal(getIndex(arr, target), 0);
         });
     });
 });
